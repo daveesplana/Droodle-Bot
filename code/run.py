5bee74bde87d8ba50311eb9760a8895dc78104b8
Dave <94614523+sussyybaka@users.noreply.github.com>
2022-12-31 15:57:46 +0800

    Update run.py

--- code/run.py
+++ code/run.py
@@ -2,6 +2,18 @@ import discord
 
 @bot.event
 async def on_ready():
-  print('Online')
-
+    await bot.change_presence(
+        activity=discord.Activity(
+            type=discord.ActivityType.listening,
+            name="you"))
+    print(f'Logging in as {bot.id}')
+    print('Online!')
+    
+@slash.slash(
+  title="ping",
+  description="pong!"
+)
+async def ping(self, ctx):
+  await ctx.send('Pong!')
+    
 bot.run(token)
