c26c643b0805d296fb582f9451e7559ac6bcc8ad
Dave <94614523+sussyybaka@users.noreply.github.com>
2023-01-01 13:45:31 +0800

    Update run.py
    title changed to name

--- code/run.py
+++ code/run.py
@@ -2,15 +2,11 @@ import discord
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(
-        activity=discord.Activity(
-            type=discord.ActivityType.listening,
-            name="you"))
     print(f'Logging in as {bot.id}')
     print('Online!')
     
 @slash.slash(
-  title="ping",
+  name="ping",
   description="pong!"
 )
 async def ping(self, ctx):
