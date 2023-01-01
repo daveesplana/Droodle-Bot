cc37783edc4cb1d30d7cc38b1ba186f8dfd3e52e
Dave <94614523+sussyybaka@users.noreply.github.com>
2023-01-01 13:53:14 +0800

    Create commands.py

---
+++ code/commands.py
@@ -0,0 +1,45 @@
+import discord
+import os
+import random
+from googletrans import Translator
+from discord import Client, Intents, Embed
+from discord_slash import SlashCommand, SlashContext
+
+bot = Client(intents=Intents.default())
+slash = SlashCommand(bot)
+determine_flip = [1, 0]
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(
+        activity=discord.Activity(
+            type=discord.ActivityType.listening,
+            name="you"))
+    print(f'Logging in as {bot.id}')
+    print('Online!')
+
+@slash.slash(name="echo", description="use this command for fun")    
+async def echo(ctx, *, content:str):
+    embed = discord.Embed(title=f"{ctx.author.name} says",description=(content), color=0x42F56C)
+    await ctx.send(embed=embed)
+
+@slash.slash(name="ping", description="Bot Latency")
+async def ping(ctx):
+   embed = discord.Embed(title="Pong!", description="Latency: {0}".format(bot.latency), color=0x08bee2)
+   await ctx.send(embed=embed)
+
+@slash.slash(name="whoami", description="Displays your username")
+async def whoami(ctx):
+    embed=discord.Embed(title=f'{ctx.author.name}', 
+    description=f'ID: {ctx.guild.id}', 
+    color=0x83e50b)
+    await ctx.send(embed=embed)
+
+@slash.slash(name="translate", description="Translate a thing")
+async def translate(ctx, lang, *, thing):
+    translator = Translator()
+    translation = translator.translate(thing, dest=lang)
+    embed=discord.Embed(title="Translated to", description=(translation.text), color=0x8005fa)
+    await ctx.send(embed=embed)
+    
+bot.run(token)
