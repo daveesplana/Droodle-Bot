ba0b4fd61c4ccd8f18d8aa2fa0ad60254217f2d4
Dave <94614523+sussyybaka@users.noreply.github.com>
2023-01-07 11:37:29 +0000

    Update commands.py

--- code/commands.py
+++ code/commands.py
@@ -1,44 +1,34 @@
 import discord
+import dotenv
 import os
-import random
-from googletrans import Translator
-from discord import Client, Intents, Embed
-from discord_slash import SlashCommand, SlashContext
+from discord.ext import commands
+from discord.ext.commands import bot
+from discord import default_permissions
 
-bot = Client(intents=Intents.default())
-slash = SlashCommand(bot)
+bot = discord.Bot()
+dotenv.load_dotenv()
+token = str(os.getenv("TOKEN"))
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(
-        activity=discord.Activity(
-            type=discord.ActivityType.listening,
-            name="you"))
-    print(f'Logging in as {bot.id}')
-    print('Online!')
-
-@slash.slash(name="echo", description="use this command for fun")    
-async def echo(ctx, *, content:str):
-    embed = discord.Embed(title=f"{ctx.author.name} says",description=(content), color=0x42F56C)
-    await ctx.send(embed=embed)
-
-@slash.slash(name="ping", description="Bot Latency")
-async def ping(ctx):
-   embed = discord.Embed(title="Pong!", description="Latency: {0}".format(bot.latency), color=0x08bee2)
-   await ctx.send(embed=embed)
-
-@slash.slash(name="whoami", description="Displays your username")
-async def whoami(ctx):
-    embed=discord.Embed(title=f'{ctx.author.name}', 
-    description=f'ID: {ctx.guild.id}', 
-    color=0x83e50b)
-    await ctx.send(embed=embed)
-
-@slash.slash(name="translate", description="Translates a message")
-async def translate(ctx, lang, *, thing):
-    translator = Translator()
-    translation = translator.translate(thing, dest=lang)
-    embed=discord.Embed(title="Translated to", description=(translation.text), color=0x8005fa)
-    await ctx.send(embed=embed)
+    print(f"Logged in as {bot.user} ID: {bot.user.id}")
+    print("Bot Online!")
     
+@bot.command(description="Sends bot Latency.")
+async def ping(ctx):
+    embed = discord.Embed(
+        title="Pong!",
+        description=f"Latency is {bot.latency}.",
+        color=discord.Colour.blurple()
+    )
+    await ctx.respond(embed=embed)
+
+@bot.command(description="Displays your username.")
+async def whoami(ctx):
+    embed = discord.Embed(
+        title=f"{ctx.author}",
+        description=f"Creation Date: {member.created_at}"
+    )
+    await ctx.respond(embed=embed)
+
 bot.run(token)
