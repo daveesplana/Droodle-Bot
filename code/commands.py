7c01ab16b4e828d263d1c729944fde9eaeed769f
Dave <94614523+sussyybaka@users.noreply.github.com>
2023-01-29 20:46:56 +0800

    Added user id to the whoami command
    Updated the list of fields to include the user id alongside the user's status and roles.

--- code/commands.py
+++ code/commands.py
@@ -5,6 +5,8 @@ from discord.ext import commands
 from discord.ext.commands import bot
 from discord import default_permissions
 
+load_dotenv()
+
 bot = discord.Bot()
 dotenv.load_dotenv()
 token = str(os.getenv("TOKEN"))
@@ -23,16 +25,20 @@ async def ping(ctx):
     )
     await ctx.respond(embed=embed)
     
-@bot.command(name="whoami", description="Displays your username, creation date, status, and roles.")
-async def whoami(ctx):
-    member = ctx.message.author
-    embed = discord.Embed(
-        title=f"{member}",
-        description=f"Creation Date: {member.created_at}",
-        color=discord.Color.blurple()
-    )
-    embed.add_field(name="Status", value=member.status)
-    embed.add_field(name="Roles", value=", ".join([role.name for role in member.roles]))
+@bot.on_command("whoami")
+async def whoami(message):
+    member = message.author
+    fields = [
+        {"name": "Status", "value": member.status},
+        {"name": "Roles", "User ID," "value": ", ".join([role.name for role in member.roles])}
+        {"name": "User ID", "value": member.id}
+    ]
+    embed = {
+        "title": f"{member}",
+        "description": f"Creation Date: {member.created_at}",
+        "color": 0x7289da,
+        "fields": fields
+    }
     await ctx.respond(embed=embed)
 
 bot.run(token)
