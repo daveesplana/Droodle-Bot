ce41538985a71ea352c4a4f470aa9317629e213e
Dave <susssybaka@duck.com>
2023-10-01 22:04:44 +0800

    Commit changes to bot.py and run.py

--- code/bot.py
+++ code/bot.py
@@ -4,6 +4,7 @@ import os
 from discord.ext import commands
 from discord.ext.commands import bot
 from discord import default_permissions
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -25,20 +26,4 @@ async def ping(ctx):
     )
     await ctx.respond(embed=embed)
     
-@bot.on_command("whoami")
-async def whoami(message):
-    member = message.author
-    fields = [
-        {"name": "Status", "value": member.status},
-        {"name": "Roles", "User ID," "value": ", ".join([role.name for role in member.roles])}
-        {"name": "User ID", "value": member.id}
-    ]
-    embed = {
-        "title": f"{member}",
-        "description": f"Creation Date: {member.created_at}",
-        "color": 0x7289da,
-        "fields": fields
-    }
-    await ctx.respond(embed=embed)
-
 bot.run(token)
