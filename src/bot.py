2f7c45b371313a92499829886298d1c6f7d408a7
Dave <94614523+daveesplana@users.noreply.github.com>
2023-10-13 16:16:45 +0800

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -41,56 +41,56 @@ class MyBot(discord.Bot):
                         description=f"{message}"
                         )
                 await ctx.respond("Here's the message.", embed=embed)
-                
-                @bot.command(description="Gives users information.")
-                async def userinfo(ctx, user:discord.User):
-                    user_id = user.id
-                    username = user.name
-                    discriminator = user.discriminator
-                    created_at = user.created_at.strftime('%B %d, %Y')
-                    joined_at = user.joined_at.strftime('%B %d, %Y')
-                    embed = discord.Embed(
-                            title=f"{username}",
-                            description=f"{user_id}",
-                            color = discord.Color.green()
-                            )
-                    embed.add_field(
-                            name="Account Created:", 
-                            value=f"`{created_at}`", 
-                            inline=True
-                            )
-                    embed.add_field(
-                            name="Date Joined:", 
-                            value=f"`{joined_at}`", 
-                            inline=True
-                            )
-                    await ctx.respond(embed=embed)
-                    
-                    @bot.command(description="Allows the bot to join a channel.")
-                    async def join(ctx):
-                        if not ctx.author.voice:
-                            await ctx.respond("You must be in a voice channel.")
-                            return
-                        channel = ctx.author.voice.channel
-                        voice_channel = await channel.connect()
+
+            @bot.command(description="Gives users information.")
+            async def userinfo(ctx, user:discord.User):
+                user_id = user.id
+                username = user.name
+                discriminator = user.discriminator
+                created_at = user.created_at.strftime('%B %d, %Y')
+                joined_at = user.joined_at.strftime('%B %d, %Y')
+                embed = discord.Embed(
+                    title=f"{username}",
+                    description=f"{user_id}",
+                    color = discord.Color.green()
+                )
+                embed.add_field(
+                    name="Account Created:",
+                    value=f"`{created_at}`",
+                    inline=True
+                )
+                embed.add_field(
+                    name="Date Joined:",
+                    value=f"`{joined_at}`",
+                    inline=True
+                )
+                await ctx.respond(embed=embed)
+
+            @bot.command(description="Allows the bot to join a channel.")
+            async def join(ctx):
+                if not ctx.author.voice:
+                    await ctx.respond("You must be in a voice channel.")
+                    return
+                channel = ctx.author.voice.channel
+                voice_channel = await channel.connect()
+                embed = discord.Embed(
+                        title=f"Joined {channel.name}"
+                        )
+                await ctx.respond(embed=embed)
+
+                @bot.command(description="Allows the bot to leave a voice channel.")
+                async def leave(ctx):
+                    if ctx.voice_client:
                         embed = discord.Embed(
-                                title=f"Joined {channel.name}"
+                                title="Left the voice channel"
+                                )
+                        await ctx.voice_client.disconnect()
+                        await ctx.respond(embed=embed)
+                    else:
+                        embed = discord.Embed(
+                                title="I'm not in a voice channel"
                                 )
                         await ctx.respond(embed=embed)
-                        
-                        @bot.command(description="Allows the bot to leave a voice channel.")
-                        async def leave(ctx):
-                            if ctx.voice_client:
-                                embed = discord.Embed(
-                                        title="Left the voice channel"
-                                        )
-                                await ctx.voice_client.disconnect()
-                                await ctx.respond(embed=embed)
-                            else:
-                                embed = discord.Embed(
-                                        title="I'm not in a voice channel"                  
-                                        )
-                                await ctx.respond(embed=embed)
 
 bot = MyBot()
 
